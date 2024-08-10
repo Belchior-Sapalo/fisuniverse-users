34c4115fa81f4d4b76b5c4be564ee5e24f0ad14b
Belchior <belchiorbenilson@gmail.com>
2024-08-10 14:22:12 +0100

    atualizei

--- src/pages/anexos/anexos.js
+++ src/pages/anexos/anexos.js
@@ -1,39 +1,7 @@
-import React from "react";
-import '../anexos/anexos.css'
-import Navbar from "../../components/navbar/navbar";
-import Card from "../../components/card/card";
-import cover1 from '../../assets/images/back6.jpg'
+
 
 export default function Anexos(){
 	return(
-		<section id="anexos-section">
-			<div className="row">
-				<Card 
-					image={cover1}
-					title="Fisica das coisas" description="livro de fisica" 
-					ano="2024"
-				/>
-				<Card 
-					image={cover1}
-					title="Fisica das coisas" description="livro de fisica" 
-					ano="2024"
-				/>
-				<Card 
-					image={cover1}
-					title="Fisica das coisas" description="livro de fisica" 
-					ano="2024"
-				/>
-				<Card 
-					image={cover1}
-					title="Fisica das coisas" description="livro de fisica" 
-					ano="2024"
-				/>
-				<Card 
-					image={cover1}
-					title="Fisica das coisas" description="livro de fisica" 
-					ano="2024"
-				/>
-			</div>
-		</section>
+		<h1>Anexos</h1>
 	)
 }
